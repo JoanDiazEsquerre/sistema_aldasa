3d0563cb96952a3428482b6e480dbd5a8b09a4ce
Jhon Diaz <jw.diaz.es@gmail.com>
2022-09-06 10:54:05 -0500

    rutas dinamicas

---
+++ src/main/java/com/model/pavita/general/bean/NavegacionBean.java
@@ -0,0 +1,44 @@
+package com.model.pavita.general.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@ManagedBean
+@SessionScoped
+public class NavegacionBean {
+	
+	private String ruta;;
+	
+
+	@PostConstruct
+	public void init() {
+
+		ruta="modulos/prospeccion/procesos/prospeccion.xhtml";
+	}
+	
+	public void getProcesoProspeccionPage() {
+       ruta="modulos/prospeccion/procesos/prospeccion.xhtml";
+    }
+	
+	public void getMantenimientoPersonasPage() {
+        ruta = "modulos/prospeccion/mantenimientos/personas.xhtml";
+    }
+
+
+	public String getRuta() {
+		return ruta;
+	}
+
+
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+
+
+	
+
+}
