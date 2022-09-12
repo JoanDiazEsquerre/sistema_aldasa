66669906bf7f83f90b2bc429800b8b9c28ed06e0
Alexis-Esquerre <ja.diazesquerre@gmail.com>
2022-09-12 17:29:38 -0500

    avance

--- src/main/java/com/model/aldasa/general/bean/ProfileBean.java
+++ src/main/java/com/model/aldasa/general/bean/ProfileBean.java
@@ -17,46 +17,51 @@ import com.model.aldasa.service.ProfileService;
 @Component
 @ManagedBean
 @SessionScoped
-public class ProfileBean {
 
+public class ProfileBean {
+	
 	@Autowired
 	private ProfileService profileService;
 	
-	private List<Profile> listProfile;
+	private List<Profile> ListProfile;
+	private Profile profileSelected;
 	
 	@PostConstruct
-	public void init()  {
-		listProfile=profileService.findByStatus(true);
-		/*Profile profile1 = new Profile();
-		profile1.setName("Admin");
-		profile1.setId(1);
-		
-		Profile profile2 = new Profile();
-		profile2.setName("Asesor");
-		profile2.setId(2);
-		
-		listProfile = new ArrayList<>();
-		listProfile.add(profile1);
-		listProfile.add(profile2);*/
+	public void init() {
+		ListProfile=profileService.findByStatus(true);
 	}
-
+	public void newProfile() {
+		profileSelected=new Profile();
+		profileSelected.setStatus(true);
+		profileSelected.setName("");
+	}
+	
+	public void saveProfile() {
+		profileService.save(profileSelected);
+		ListProfile=profileService.findByStatus(true);
+	}
+	
 	public ProfileService getProfileService() {
 		return profileService;
 	}
-
+	
 	public void setProfileService(ProfileService profileService) {
 		this.profileService = profileService;
 	}
-
-	public List<Profile> getListProfile() {
-		return listProfile;
-	}
-
-	public void setListProfile(List<Profile> listProfile) {
-		this.listProfile = listProfile;
-	}
-
-
 	
+	public List<Profile> getListProfile() {
+		return ListProfile;
+	}
+	
+	public void setListProfile(List<Profile> listProfile) {
+		ListProfile = listProfile;
+	}
+	
+	public Profile getProfileSelected() {
+		return profileSelected;
+	}
+	public void setProfileSelected(Profile profileSelected) {
+		this.profileSelected = profileSelected;
+	}
 	
 }
