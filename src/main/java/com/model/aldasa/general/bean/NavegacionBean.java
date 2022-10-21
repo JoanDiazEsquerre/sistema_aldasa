aa4a68fde90792557f21d4e35a01e0673689ed37
JoanDiazEsquerre~ <joanjesusanthony@gmail.com>
2022-10-21 15:44:57 -0500

    21/10

--- src/main/java/com/model/aldasa/general/bean/NavegacionBean.java
+++ src/main/java/com/model/aldasa/general/bean/NavegacionBean.java
@@ -3,7 +3,7 @@ package com.model.aldasa.general.bean;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.inject.Inject;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import com.model.aldasa.entity.Usuario;
-import com.model.aldasa.prospeccion.bean.AgendaBean;
-import com.model.aldasa.prospeccion.bean.ProspeccionBean;
-import com.model.aldasa.prospeccion.bean.ProspectoBean;
-import com.model.aldasa.prospeccion.bean.ReporteProspeccionBean;
+
 import com.model.aldasa.service.UsuarioService;
+import com.model.aldasa.util.Perfiles;
 
 @Named
 @Component
@@ -30,12 +28,88 @@ public class NavegacionBean {
 	private String ruta;
 	private String username;
 	private Usuario usuarioLogin = new Usuario();
+	
+	private int idAdministrador = Perfiles.ADMINISTRADOR.getId();
+	private int idSupervisor = Perfiles.SUPERVISOR.getId();
+	private int idAsesor = Perfiles.ASESOR.getId();
+	
+	private boolean menuProspeccion, menuMantenimiento,menuReporte;
+	private boolean subMenuProspectos, subMenuProspeccion,subMenuAgenda,subMenuPersonas,subMenuUsuarios,subMenuPerfiles, 
+					subMenuProyectos,subMenuEquipos,subMenuCambiarContrasenia, subMenuReporteAcciones;
+	
+	private int[] permisoProspectos= {idAdministrador,idSupervisor,idAsesor};
+	private int[] permisoProspeccion= {idAdministrador,idSupervisor,idAsesor};
+	private int[] permisoAgenda= {idAdministrador,idSupervisor,idAsesor};
+	private int[] permisoPersonas= {idAdministrador};
+	private int[] permisoUsuarios= {idAdministrador};
+	private int[] permisoPerfiles= {idAdministrador};
+	private int[] permisoProyectos= {idAdministrador};
+	private int[] permisoEquipos= {idAdministrador};
+	private int[] permisoCambiarConstrasenia= {idAdministrador,idSupervisor,idAsesor};
+	private int[] permisoReporteAcciones= {idAdministrador,idSupervisor,idAsesor};
+
 
 	@PostConstruct
 	public void init() {
 		ruta = "modulos/general/mantenimientos/inicio.xhtml";
 		
 	}
+	
+	public void onPageLoad(){
+		usuarioLogin = usuarioService.findByUsername(getUsername());
+		permisoPantallas();
+		
+	}
+	
+	
+	public void permisoPantallas() {
+		menuProspeccion=false; menuMantenimiento=false; menuReporte=false;
+		
+		//*******************************************************************************
+		subMenuProspectos = validaPermiso(permisoProspectos);
+		subMenuProspeccion= validaPermiso(permisoProspeccion);
+		subMenuAgenda= validaPermiso(permisoAgenda);
+		
+		if(subMenuProspectos || subMenuProspeccion || subMenuAgenda) {
+			menuProspeccion=true;
+		}
+		
+		//******************************************************************************
+		subMenuPersonas = validaPermiso(permisoPersonas);
+		subMenuUsuarios= validaPermiso(permisoUsuarios);
+		subMenuPerfiles= validaPermiso(permisoPerfiles);
+		subMenuProyectos= validaPermiso(permisoProyectos);
+		subMenuEquipos= validaPermiso(permisoEquipos);
+		subMenuCambiarContrasenia= validaPermiso(permisoCambiarConstrasenia);
+		
+		if(subMenuPersonas || subMenuUsuarios || subMenuPerfiles || subMenuProyectos || subMenuEquipos || subMenuCambiarContrasenia) {
+			menuMantenimiento=true;
+		}
+		
+		//*******************************************************************************
+		subMenuReporteAcciones = validaPermiso(permisoReporteAcciones);
+		
+		if(subMenuReporteAcciones) {
+			menuReporte=true;
+		}
+				
+		
+	}
+	
+	public boolean validaPermiso(int[] permiso) {
+		boolean valida = false;
+		for(int per:permiso) {
+			if(per == usuarioLogin.getProfile().getId()) {
+				valida= true;
+			}
+		}
+		
+		return valida;
+	}
+	
+	
+	
+	
 	
 	public void getProcesoProspeccionPage() {
        ruta="modulos/prospeccion/procesos/prospeccion.xhtml";
@@ -78,16 +152,21 @@ public class NavegacionBean {
     }
 	
 	
-	public void onPageLoad(){
-		usuarioLogin = usuarioService.findByUsername(getUsername());
-		
+	
+	
+	
+	public void cerrarSesion() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 	}
+	
+	
+	
+	
+	
 
 	public String getRuta() {
 		return ruta;
 	}
-
-
 	public void setRuta(String ruta) {
 		this.ruta = ruta;
 	}
@@ -118,6 +197,214 @@ public class NavegacionBean {
 
 	public void setUsuarioLogin(Usuario usuarioLogin) {
 		this.usuarioLogin = usuarioLogin;
+	}
+
+	public int getIdAdministrador() {
+		return idAdministrador;
+	}
+
+	public void setIdAdministrador(int idAdministrador) {
+		this.idAdministrador = idAdministrador;
+	}
+
+	public int getIdSupervisor() {
+		return idSupervisor;
+	}
+
+	public void setIdSupervisor(int idSupervisor) {
+		this.idSupervisor = idSupervisor;
+	}
+
+	public int getIdAsesor() {
+		return idAsesor;
+	}
+
+	public void setIdAsesor(int idAsesor) {
+		this.idAsesor = idAsesor;
+	}
+
+	public boolean isMenuProspeccion() {
+		return menuProspeccion;
+	}
+
+	public void setMenuProspeccion(boolean menuProspeccion) {
+		this.menuProspeccion = menuProspeccion;
+	}
+
+	public boolean isMenuMantenimiento() {
+		return menuMantenimiento;
+	}
+
+	public void setMenuMantenimiento(boolean menuMantenimiento) {
+		this.menuMantenimiento = menuMantenimiento;
+	}
+
+	public boolean isMenuReporte() {
+		return menuReporte;
+	}
+
+	public void setMenuReporte(boolean menuReporte) {
+		this.menuReporte = menuReporte;
+	}
+
+	public boolean isSubMenuProspectos() {
+		return subMenuProspectos;
+	}
+
+	public void setSubMenuProspectos(boolean subMenuProspectos) {
+		this.subMenuProspectos = subMenuProspectos;
+	}
+
+	public boolean isSubMenuProspeccion() {
+		return subMenuProspeccion;
+	}
+
+	public void setSubMenuProspeccion(boolean subMenuProspeccion) {
+		this.subMenuProspeccion = subMenuProspeccion;
+	}
+
+	public boolean isSubMenuAgenda() {
+		return subMenuAgenda;
+	}
+
+	public void setSubMenuAgenda(boolean subMenuAgenda) {
+		this.subMenuAgenda = subMenuAgenda;
+	}
+
+	public boolean isSubMenuPersonas() {
+		return subMenuPersonas;
+	}
+
+	public void setSubMenuPersonas(boolean subMenuPersonas) {
+		this.subMenuPersonas = subMenuPersonas;
+	}
+
+	public boolean isSubMenuUsuarios() {
+		return subMenuUsuarios;
+	}
+
+	public void setSubMenuUsuarios(boolean subMenuUsuarios) {
+		this.subMenuUsuarios = subMenuUsuarios;
+	}
+
+	public boolean isSubMenuPerfiles() {
+		return subMenuPerfiles;
+	}
+
+	public void setSubMenuPerfiles(boolean subMenuPerfiles) {
+		this.subMenuPerfiles = subMenuPerfiles;
+	}
+
+	public boolean isSubMenuProyectos() {
+		return subMenuProyectos;
+	}
+
+	public void setSubMenuProyectos(boolean subMenuProyectos) {
+		this.subMenuProyectos = subMenuProyectos;
+	}
+
+	public boolean isSubMenuEquipos() {
+		return subMenuEquipos;
+	}
+
+	public void setSubMenuEquipos(boolean subMenuEquipos) {
+		this.subMenuEquipos = subMenuEquipos;
+	}
+
+	public boolean isSubMenuCambiarContrasenia() {
+		return subMenuCambiarContrasenia;
+	}
+
+	public void setSubMenuCambiarContrasenia(boolean subMenuCambiarContrasenia) {
+		this.subMenuCambiarContrasenia = subMenuCambiarContrasenia;
+	}
+
+	public boolean isSubMenuReporteAcciones() {
+		return subMenuReporteAcciones;
+	}
+
+	public void setSubMenuReporteAcciones(boolean subMenuReporteAcciones) {
+		this.subMenuReporteAcciones = subMenuReporteAcciones;
+	}
+
+	public int[] getPermisoProspectos() {
+		return permisoProspectos;
+	}
+
+	public void setPermisoProspectos(int[] permisoProspectos) {
+		this.permisoProspectos = permisoProspectos;
+	}
+
+	public int[] getPermisoProspeccion() {
+		return permisoProspeccion;
+	}
+
+	public void setPermisoProspeccion(int[] permisoProspeccion) {
+		this.permisoProspeccion = permisoProspeccion;
+	}
+
+	public int[] getPermisoAgenda() {
+		return permisoAgenda;
+	}
+
+	public void setPermisoAgenda(int[] permisoAgenda) {
+		this.permisoAgenda = permisoAgenda;
+	}
+
+	public int[] getPermisoPersonas() {
+		return permisoPersonas;
+	}
+
+	public void setPermisoPersonas(int[] permisoPersonas) {
+		this.permisoPersonas = permisoPersonas;
+	}
+
+	public int[] getPermisoUsuarios() {
+		return permisoUsuarios;
+	}
+
+	public void setPermisoUsuarios(int[] permisoUsuarios) {
+		this.permisoUsuarios = permisoUsuarios;
+	}
+
+	public int[] getPermisoPerfiles() {
+		return permisoPerfiles;
+	}
+
+	public void setPermisoPerfiles(int[] permisoPerfiles) {
+		this.permisoPerfiles = permisoPerfiles;
+	}
+
+	public int[] getPermisoProyectos() {
+		return permisoProyectos;
+	}
+
+	public void setPermisoProyectos(int[] permisoProyectos) {
+		this.permisoProyectos = permisoProyectos;
+	}
+
+	public int[] getPermisoEquipos() {
+		return permisoEquipos;
+	}
+
+	public void setPermisoEquipos(int[] permisoEquipos) {
+		this.permisoEquipos = permisoEquipos;
+	}
+
+	public int[] getPermisoCambiarConstrasenia() {
+		return permisoCambiarConstrasenia;
+	}
+
+	public void setPermisoCambiarConstrasenia(int[] permisoCambiarConstrasenia) {
+		this.permisoCambiarConstrasenia = permisoCambiarConstrasenia;
+	}
+
+	public int[] getPermisoReporteAcciones() {
+		return permisoReporteAcciones;
+	}
+
+	public void setPermisoReporteAcciones(int[] permisoReporteAcciones) {
+		this.permisoReporteAcciones = permisoReporteAcciones;
 	}
 
 
