4e5b55f9534aec63fefe1a8d995e29808bf4f3fb
Jhon Diaz <jhon.diazesquerre@externos-cl.cencosud.com>
2022-11-01 12:30:58 -0500

    resolve session

--- src/main/java/com/model/aldasa/SecurityConfig.java
+++ src/main/java/com/model/aldasa/SecurityConfig.java
@@ -1,12 +1,5 @@
 package com.model.aldasa;
 
-import java.io.IOException;
-
-import javax.inject.Inject;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,31 +7,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-<<<<<<< HEAD
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-=======
->>>>>>> 9541d95084344c775346e7d655b17faedf6f48a8
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import com.model.aldasa.general.bean.NavegacionBean;
 import com.model.aldasa.service.impl.UserDetailService;
-
-import java.io.IOException;
-
-import javax.inject.Inject;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -50,13 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
-<<<<<<< HEAD
 	@Autowired
 	private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
-=======
-	@Inject
-	private NavegacionBean navegacionBean;
->>>>>>> 9541d95084344c775346e7d655b17faedf6f48a8
+
 	
 	@Bean
     public BCryptPasswordEncoder passwordEncoder() {
