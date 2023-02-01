263924d95b5c88c48a1d72109760affb481c9c30
Alexis-Esquerre <ja.diazesquerre@gmail.com>
2023-02-01 10:37:09 -0500

    0102

--- src/main/java/com/model/aldasa/ventas/bean/RankingVentasBean.java
+++ src/main/java/com/model/aldasa/ventas/bean/RankingVentasBean.java
@@ -1,12 +1,23 @@
 package com.model.aldasa.ventas.bean;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.model.FilterMeta;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortMeta;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.model.aldasa.entity.Empleado;
 import com.model.aldasa.service.EmpleadoService;
 
 @ManagedBean
@@ -18,12 +29,79 @@ public class RankingVentasBean implements Serializable {
 	@ManagedProperty(value = "#{empleadoService}")
 	private EmpleadoService empleadoService;
 	
+	private LazyDataModel<Empleado> lstEmpleadoLazy;
+	
+	private Empleado empleadoSelected;
+	
+	private boolean estado = true;
+
+
+	
 	@PostConstruct
 	public void init() {
 	
 	}
 
 	
+	
+	public void iniciarLazy() {
+
+		lstEmpleadoLazy = new LazyDataModel<Empleado>() {
+			private List<Empleado> datasource;
+
+            @Override
+            public void setRowIndex(int rowIndex) {
+                if (rowIndex == -1 || getPageSize() == 0) {
+                    super.setRowIndex(-1);
+                } else {
+                    super.setRowIndex(rowIndex % getPageSize());
+                }
+            }
+
+            @Override
+            public Empleado getRowData(String rowKey) {
+                int intRowKey = Integer.parseInt(rowKey);
+                for (Empleado empleado : datasource) {
+                    if (empleado.getId() == intRowKey) {
+                        return empleado;
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            public String getRowKey(Empleado empleado) {
+                return String.valueOf(empleado.getId());
+            }
+
+			@Override
+			public List<Empleado> load(int first, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
+               
+				String names = "%" + (filterBy.get("person.surnames") != null ? filterBy.get("person.surnames").getFilterValue().toString().trim().replaceAll(" ", "%") : "") + "%";
+
+                Sort sort=Sort.by("person.surnames").ascending();
+                if(sortBy!=null) {
+                	for (Map.Entry<String, SortMeta> entry : sortBy.entrySet()) {
+                	   if(entry.getValue().getOrder().isAscending()) {
+                		   sort = Sort.by(entry.getKey()).descending();
+                	   }else {
+                		   sort = Sort.by(entry.getKey()).ascending();
+                		   
+                	   }
+                	}
+                }        
+                Pageable pageable = PageRequest.of(first/pageSize, pageSize,sort);
+               
+                Page<Empleado> pageEmpleado=null;
+               
+                
+                pageEmpleado= empleadoService.findByPersonSurnamesLikeAndEstado(names, estado, pageable);
+                
+                setRowCount((int) pageEmpleado.getTotalElements());
+                return datasource = pageEmpleado.getContent();
+            }
+		};
+	}
 	
 	
 	
@@ -32,6 +110,24 @@ public class RankingVentasBean implements Serializable {
 	}
 	public void setEmpleadoService(EmpleadoService empleadoService) {
 		this.empleadoService = empleadoService;
+	}
+	public LazyDataModel<Empleado> getLstEmpleadoLazy() {
+		return lstEmpleadoLazy;
+	}
+	public void setLstEmpleadoLazy(LazyDataModel<Empleado> lstEmpleadoLazy) {
+		this.lstEmpleadoLazy = lstEmpleadoLazy;
+	}
+	public Empleado getEmpleadoSelected() {
+		return empleadoSelected;
+	}
+	public void setEmpleadoSelected(Empleado empleadoSelected) {
+		this.empleadoSelected = empleadoSelected;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
 	}
 	
 	
